5251bc5520bdc4e26156d9ed5cddea11da20b92e
lefrenchywtf <raphael3029@gmail.com>
2024-11-12 15:38:03 +0100

    Modulaire pour plusieurs extensions (pas encore completement implémenté)

--- LocalizationManager/LocalizationManagerTool/CodeRaphael.cs
+++ LocalizationManager/LocalizationManagerTool/CodeRaphael.cs
@@ -25,42 +25,66 @@ namespace LocalizationManagerTool
             if (openFileDialog.ShowDialog() == true)
             {
                 string FileName = openFileDialog.FileName;
+                string extension = FileName.Split('.')[1];
                 using (System.IO.StreamReader streamReader = new StreamReader(FileName))
                 {
-                    Columns.Clear();
-                    string[] parameters = streamReader.ReadLine().Split(";");
-                    foreach (string parameter in parameters)
+                    ClearTab();
+                    switch(extension)
                     {
-                        Columns.Add(parameter);
-                    }
-                    dataTable.Columns.Clear();
-                    dataTable.Rows.Clear();
-
-                    dataGrid.Columns.Clear();
-                    dataGrid.ItemsSource = null;
-                    foreach (string column in Columns)
-                    {
-                        //Pour ajouter une colonne à notre datagrid
-                        DataColumn textColumn = new DataColumn();
-                        
-                        textColumn.ColumnName = column;
-                        dataTable.Columns.Add(textColumn);
-                    }
-
-                    while (!streamReader.EndOfStream)
-                    {
-                        string[] test = streamReader.ReadLine().Split(';');
-                        dataTable.Rows.Add(test);
-
-                        //foreach(DataRow textColumn in dataTable.Columns)
-                        //{
-                        //    Debug.WriteLine(textColumn[0]);
-                        //}
-                        //dataGrid.Items.Add(;
+                        case "csv":
+                            ReadCSV(streamReader);
+                            break;
+                        case "xml":
+                            ReadXML(streamReader);
+                            break;
                     }
                     dataGrid.ItemsSource = dataTable.DefaultView;
                 }
             }
+        }
+
+        void ReadCSV(StreamReader streamReader)
+        {
+            Columns.Clear();
+            string[] parameters = streamReader.ReadLine().Split(";");
+            foreach (string parameter in parameters)
+            {
+                Columns.Add(parameter);
+            }
+            foreach (string column in Columns)
+            {
+                //Pour ajouter une colonne à notre datagrid
+                DataColumn textColumn = new DataColumn();
+
+                textColumn.ColumnName = column;
+                dataTable.Columns.Add(textColumn);
+            }
+
+            while (!streamReader.EndOfStream)
+            {
+                string[] test = streamReader.ReadLine().Split(';');
+                dataTable.Rows.Add(test);
+
+                //foreach(DataRow textColumn in dataTable.Columns)
+                //{
+                //    Debug.WriteLine(textColumn[0]);
+                //}
+                //dataGrid.Items.Add(;
+            }
+        }
+
+        void ReadXML(StreamReader streamReader)
+        {
+
+        }
+
+        void ClearTab()
+        {
+            dataTable.Columns.Clear();
+            dataTable.Rows.Clear();
+
+            dataGrid.Columns.Clear();
+            dataGrid.ItemsSource = null;
         }
     }
 
